d3b07b869251a59af4be230f785f632b476d0253
Doremi203 <maxim.r.ivanov@gmail.com>
2024-05-28 19:23:47 +0300

    Implemented auth service

--- authenticationService/domain/src/main/kotlin/org/amogus/authenticationservice/domain/interfaces/AuthenticationService.kt
+++
@@ -1,9 +0,0 @@
-package org.amogus.authenticationservice.domain.interfaces
-
-import org.amogus.authenticationservice.domain.models.AuthenticationResult
-import org.amogus.authenticationservice.domain.models.Credentials
-
-interface AuthenticationService {
-    fun register(request: Credentials): AuthenticationResult?
-    fun login(request: Credentials): AuthenticationResult?
-}
---
+++ authenticationService/domain/src/main/kotlin/org/amogus/authenticationservice/domain/interfaces/services/AuthenticationService.kt
@@ -0,0 +1,9 @@
+package org.amogus.authenticationservice.domain.interfaces.services
+
+import org.amogus.authenticationservice.domain.models.AuthenticationResult
+import org.amogus.authenticationservice.domain.models.Credentials
+
+interface AuthenticationService {
+    suspend fun register(credentials: Credentials): AuthenticationResult?
+    suspend fun login(credentials: Credentials): AuthenticationResult?
+}
