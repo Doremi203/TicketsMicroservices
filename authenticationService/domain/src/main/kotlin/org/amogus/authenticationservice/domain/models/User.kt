18cd491eb7dd408e843bb6a300ba8cd92f33205f
Doremi203 <maxim.r.ivanov@gmail.com>
2024-05-28 19:19:19 +0300

    Changed user fields visibility

--- authenticationService/domain/src/main/kotlin/org/amogus/authenticationservice/domain/models/User.kt
+++ authenticationService/domain/src/main/kotlin/org/amogus/authenticationservice/domain/models/User.kt
@@ -1,7 +1,7 @@
 package org.amogus.authenticationservice.domain.models
 
 data class User(
-    private val username: String,
-    private val password: String,
+    val username: String,
+    val password: String,
     val id: Long = 0L,
 )
