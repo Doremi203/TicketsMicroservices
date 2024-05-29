c304041a4515585f8010cf729473305f0e6c53c4
Doremi203 <maxim.r.ivanov@gmail.com>
2024-05-29 10:47:38 +0300

    Added required dependencies

--- authenticationService/data/build.gradle.kts
+++ authenticationService/data/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     api(project(":authenticationService:domain"))
 
+    implementation("io.r2dbc:r2dbc-pool")
     implementation("org.postgresql:r2dbc-postgresql")
+    implementation("org.liquibase:liquibase-core")
 }
