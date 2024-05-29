faa50b537109b2441573875e1930e67da961ce96
Doremi203 <maxim.r.ivanov@gmail.com>
2024-05-29 18:58:00 +0300

    Added user entity and table mapping

--- authenticationService/domain/src/main/kotlin/org/amogus/authenticationservice/domain/interfaces/repositories/UserRepository.kt
+++ authenticationService/domain/src/main/kotlin/org/amogus/authenticationservice/domain/interfaces/repositories/UserRepository.kt
@@ -1,9 +1,10 @@
 package org.amogus.authenticationservice.domain.interfaces.repositories
 
+import org.amogus.authenticationservice.domain.entities.UserEntityV1
 import org.amogus.authenticationservice.domain.models.User
 import org.amogus.authenticationservice.domain.types.Email
 
 interface UserRepository {
-    suspend fun add(user: User): Int
-    suspend fun getByEmail(email: Email): User?
+    suspend fun add(users: List<UserEntityV1>): List<Int>
+    suspend fun getByEmail(email: Email): UserEntityV1?
 }
