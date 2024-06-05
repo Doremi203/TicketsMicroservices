f497bed63ce30414ff0b3d131ed5ed89a473c803
Doremi203 <maxim.r.ivanov@gmail.com>
2024-06-05 12:30:53 +0300

    Added not found exceptions

---
+++ authentication-service/domain/src/main/kotlin/org/amogus/authenticationservice/domain/exceptions/UserNotFoundException.kt
@@ -0,0 +1,15 @@
+package org.amogus.authenticationservice.domain.exceptions
+
+import org.amogus.authenticationservice.domain.types.Email
+import org.amogus.authenticationservice.domain.types.UserId
+
+class UserNotFoundException : NotFoundException {
+    constructor(email: Email) : this(email, null)
+    constructor(email: Email, cause: Throwable?) : super("User with id ${email.value} not found", cause)
+    constructor(email: Email, cause: Throwable?, enableSuppression: Boolean, writableStackTrace: Boolean) : super(
+        "User with id ${email.value} not found",
+        cause,
+        enableSuppression,
+        writableStackTrace
+    )
+}
