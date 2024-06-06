a212b10d5ecea57d0d18067a132c3350071b1194
Doremi203 <maxim.r.ivanov@gmail.com>
2024-06-06 22:47:24 +0300

    Fixed jwt auth

--- authentication-service/api/src/main/kotlin/org/amogus/authenticationservice/api/configuration/SecurityConfig.kt
+++ authentication-service/api/src/main/kotlin/org/amogus/authenticationservice/api/configuration/SecurityConfig.kt
@@ -1,23 +1,38 @@
 package org.amogus.authenticationservice.api.configuration
 
+import org.amogus.authenticationservice.api.filters.JwtAuthenticationFilter
+import org.amogus.authenticationservice.domain.interfaces.services.JwtService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.config.web.server.invoke
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
 
 @Configuration
 @EnableWebFluxSecurity
 class SecurityConfig {
     @Bean
-    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        return http {
-            authorizeExchange {
-                authorize(anyExchange, permitAll)
+    fun springSecurityFilterChain(
+        http: ServerHttpSecurity,
+        authenticationManager: ReactiveAuthenticationManager,
+        jwtService: JwtService
+    ): SecurityWebFilterChain {
+        val jwtAuthenticationFilter = JwtAuthenticationFilter(jwtService)
+        return http
+            .authenticationManager(authenticationManager)
+            .csrf { it.disable() }
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .logout { it.disable() }
+            .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+            .authorizeExchange {
+                it.pathMatchers("/api/auth/user-info").authenticated()
+                it.anyExchange().permitAll()
             }
-            csrf { disable() }
-            httpBasic {  }
-        }
+            .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.HTTP_BASIC)
+            .build()
     }
 }
