0f17a40ec425125dbeb4c399fc03bf901bfcfc10
Doremi203 <maxim.r.ivanov@gmail.com>
2024-06-07 12:04:56 +0300

    Configured desired auth exception entrypoint

--- authentication-service/api/src/main/kotlin/org/amogus/authenticationservice/api/configuration/SecurityConfig.kt
+++ authentication-service/api/src/main/kotlin/org/amogus/authenticationservice/api/configuration/SecurityConfig.kt
@@ -4,11 +4,13 @@ import org.amogus.authenticationservice.api.filters.JwtAuthenticationFilter
 import org.amogus.authenticationservice.domain.interfaces.services.JwtService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
 
 @Configuration
@@ -32,7 +34,10 @@ class SecurityConfig {
                 it.pathMatchers("/api/auth/user-info").authenticated()
                 it.anyExchange().permitAll()
             }
-            .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.HTTP_BASIC)
+            .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+            .exceptionHandling {
+                it.authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
+            }
             .build()
     }
 }
