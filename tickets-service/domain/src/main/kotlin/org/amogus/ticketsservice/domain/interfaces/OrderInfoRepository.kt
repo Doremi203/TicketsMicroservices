2dbfbef97631690848d8279402fef33c1d6783c6
Doremi203 <maxim.r.ivanov@gmail.com>
2024-06-09 11:52:33 +0300

    Added get order by id to repository

--- tickets-service/domain/src/main/kotlin/org/amogus/ticketsservice/domain/interfaces/OrderInfoRepository.kt
+++ tickets-service/domain/src/main/kotlin/org/amogus/ticketsservice/domain/interfaces/OrderInfoRepository.kt
@@ -1,8 +1,10 @@
 package org.amogus.ticketsservice.domain.interfaces
 
 import org.amogus.ticketsservice.domain.models.OrderInfo
+import org.amogus.ticketsservice.domain.types.OrderId
 import org.amogus.ticketsservice.domain.types.UserId
 
 interface OrderInfoRepository {
+    suspend fun getById(id: OrderId): OrderInfo?
     suspend fun getByUserId(userId: UserId): List<OrderInfo>
 }
