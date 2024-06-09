2fbd449258cb27558b44b052bed6981150286bcb
Doremi203 <maxim.r.ivanov@gmail.com>
2024-06-09 16:38:14 +0300

    Added exception handling

---
+++ tickets-service/api/src/main/kotlin/org/amogus/ticketsservice/api/controllers/GlobalExceptionHandler.kt
@@ -0,0 +1,41 @@
+package org.amogus.ticketsservice.api.controllers
+
+import org.amogus.ticketsservice.domain.exceptions.OrderNotFoundException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.reactive.resource.NoResourceFoundException
+import java.net.URI
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+    val logger: Logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
+    @ExceptionHandler(OrderNotFoundException::class)
+    fun handleOrderNotFoundException(e: OrderNotFoundException): ProblemDetail {
+        val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, "You don't have order with id ${e.orderId}.")
+        problemDetail.type = URI.create("https://datatracker.ietf.org/doc/html/rfc9110#section-15.5.5")
+
+        return problemDetail
+    }
+
+    @ExceptionHandler(NoResourceFoundException::class)
+    fun handleNoResourceFoundException(e: NoResourceFoundException): ProblemDetail {
+        val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, "Resource not found.")
+        problemDetail.type = URI.create("https://datatracker.ietf.org/doc/html/rfc9110#section-15.5.5")
+
+        return problemDetail
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(e: Exception): ProblemDetail {
+        logger.error("Internal server error", e)
+        val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error.")
+        problemDetail.type = URI.create("https://datatracker.ietf.org/doc/html/rfc9110#section-15.6.1")
+
+        return problemDetail
+    }
+}
