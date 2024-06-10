6c04607f27e909feb875d1946bb407c5ab1ae8c2
Doremi203 <maxim.r.ivanov@gmail.com>
2024-06-10 09:48:01 +0300

    Added logging to auth endpoint

--- authentication-service/api/src/main/kotlin/org/amogus/authenticationservice/api/controllers/AuthenticationController.kt
+++ authentication-service/api/src/main/kotlin/org/amogus/authenticationservice/api/controllers/AuthenticationController.kt
@@ -13,6 +13,8 @@ import org.amogus.authenticationservice.domain.models.RegistrationData
 import org.amogus.authenticationservice.domain.types.Email
 import org.amogus.authenticationservice.domain.types.Nickname
 import org.amogus.authenticationservice.domain.types.Password
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -21,6 +23,8 @@ class AuthenticationController(
     private val authenticationService: AuthenticationService,
     private val jwtService: JwtService
 ) : AuthenticationApi {
+    private val logger: Logger = LoggerFactory.getLogger(AuthenticationController::class.java)
+
     @PostMapping("/register")
     override suspend fun register(
         @Valid
@@ -63,8 +67,9 @@ class AuthenticationController(
         @RequestHeader("Authorization")
         authHeader: String
     ): ResponseEntity<UserInfoResponse> {
+        logger.info("Getting user info")
         val userInfo = authenticationService.getUserInfo(jwtService.extractTokenFromHeader(authHeader))
-
+        logger.info("Successfully got user info")
         return ResponseEntity.ok(
             UserInfoResponse(
                 userInfo.nickname.value,
