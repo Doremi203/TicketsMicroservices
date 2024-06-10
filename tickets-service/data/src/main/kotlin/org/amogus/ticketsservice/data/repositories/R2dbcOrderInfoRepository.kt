fd20e2a371bb737ce7af0d04e7d246897eb6a4d0
Doremi203 <maxim.r.ivanov@gmail.com>
2024-06-10 12:40:02 +0300

    Added order dto to fix get order info

--- tickets-service/data/src/main/kotlin/org/amogus/ticketsservice/data/repositories/R2dbcOrderInfoRepository.kt
+++ tickets-service/data/src/main/kotlin/org/amogus/ticketsservice/data/repositories/R2dbcOrderInfoRepository.kt
@@ -1,8 +1,11 @@
 package org.amogus.ticketsservice.data.repositories
 
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import org.amogus.ticketsservice.data.OrdersTable
 import org.amogus.ticketsservice.data.StationsTable
+import org.amogus.ticketsservice.domain.dto.OrderDto
+import org.amogus.ticketsservice.domain.dto.toOrderInfo
 import org.amogus.ticketsservice.domain.interfaces.repositories.OrderInfoRepository
 import org.amogus.ticketsservice.domain.models.OrderInfo
 import org.amogus.ticketsservice.domain.types.*
@@ -17,25 +20,27 @@ class R2dbcOrderInfoRepository(
                 where OrdersTable.id eq id.value
                 and OrdersTable.userId eq userId.value)
             .fetchOneOrNull()
+            ?.toOrderInfo()
     }
 
     override suspend fun getAllByUserId(userId: UserId): List<OrderInfo> {
         return (selectOrderInfo()
                 where OrdersTable.userId eq userId.value)
             .fetchAll()
+            .map { it.toOrderInfo() }
             .toList()
     }
 
     private fun selectOrderInfo() = (dbClient selectAndBuild {
-        OrderInfo(
-            id = OrderId(it[OrdersTable.id]!!),
-            fromStationName = StationName(it[StationsTable["from"].name]!!),
-            toStationName = StationName(it[StationsTable["to"].name]!!),
-            status = OrderStatus.fromInt(it[OrdersTable.status]!!),
-            created = OrderCreationTime(it[OrdersTable.created]!!)
+        OrderDto(
+            id = it[OrdersTable.id]!!,
+            fromStationName = it[StationsTable["fr"].name]!!,
+            toStationName = it[StationsTable["t"].name]!!,
+            status = it[OrdersTable.status]!!,
+            created = it[OrdersTable.created]!!
         )
     }
             from OrdersTable
-            innerJoin StationsTable `as` "from" on OrdersTable.fromStationId eq StationsTable["from"].id
-            innerJoin StationsTable `as` "to" on OrdersTable.toStationId eq StationsTable["to"].id)
+            innerJoin StationsTable `as` "fr" on OrdersTable.fromStationId eq StationsTable["fr"].id
+            innerJoin StationsTable `as` "t" on OrdersTable.toStationId eq StationsTable["t"].id)
 }
