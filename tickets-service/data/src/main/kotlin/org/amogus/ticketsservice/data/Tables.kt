311f488644744d47f55688daf9e326c6471a8e06
Doremi203 <maxim.r.ivanov@gmail.com>
2024-06-10 09:50:16 +0300

    Fixed naming in tables mapping for tickets service

--- tickets-service/data/src/main/kotlin/org/amogus/ticketsservice/data/Tables.kt
+++ tickets-service/data/src/main/kotlin/org/amogus/ticketsservice/data/Tables.kt
@@ -11,9 +11,9 @@ object StationsTable : PostgresqlTable<StationEntityV1>("stations") {
 
 object OrdersTable : PostgresqlTable<OrderEntityV1>("orders") {
     val id = integer(OrderEntityV1::id).identity().primaryKey()
-    val userId = integer(OrderEntityV1::userId)
-    val fromStationId = integer(OrderEntityV1::fromStationId)
-    val toStationId = integer(OrderEntityV1::toStationId)
+    val userId = integer(OrderEntityV1::userId, "user_id")
+    val fromStationId = integer(OrderEntityV1::fromStationId, "from_station_id")
+    val toStationId = integer(OrderEntityV1::toStationId, "to_station_id")
     val status = integer(OrderEntityV1::status)
     val created = timestamp(OrderEntityV1::created)
 }
